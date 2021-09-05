6ba9ee03e3b92b3024afc9360c4c75d405f87f00
yogiyoazossi <chldnqja9688@naver.com>
2021-09-05 17:06:28 +0900

    2021.09.05

--- terraform_test/main.tf
+++ terraform_test/main.tf
@@ -27,11 +27,6 @@ resource "aws_instance" "example" {
   }
 }
 
-output "public_ip" {
-  value       = "aws_instance.example.public_ip"
-  description = "The Public IP address of the web server"
-}
-
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -81,6 +76,11 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
+  # ELB 연결 (optional)
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  # Instance Min/Max Count
   min_size = 2
   max_size = 10
 
@@ -88,5 +88,86 @@ resource "aws_autoscaling_group" "example" {
     key                 = "Name"
     value               = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+# ALB 생성 과정:  SG 생성 -> ALB 생성 -> Listener 생성 -> Rule 생성 -> TargetGroup 생성
+# ALB의 SG 생성
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# ALB 생성
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+# Listener 생성
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+# Target Group 생성
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+
+  }
+}
+
+# Listenr Rule 생성
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority    = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
