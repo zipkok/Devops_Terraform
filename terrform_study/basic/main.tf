2de8fe444643d7ca3bf98ffa49dc0a26635e43ef
yogiyoazossi <chldnqja9688@naver.com>
2022-03-02 18:15:45 +0900

    Update main.tf

--- terrform_study/basic/main.tf
+++ terrform_study/basic/main.tf
@@ -1,8 +1,28 @@
+# Instance 생성
 resource "aws_instance" "example" {
-    ami = "ami-0815f1607de769fae"
-    instance_type = "t2.micro"
+  ami           = "ami-0815f1607de769fae"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "terraform-example"
-    }
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World" > index.html
+                    nohup busybox httpd -f -p 8080 &
+                    EOF
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+# Security Group 생성
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
