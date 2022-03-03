a086a45a3fb7d30a185515af3739b01bdab0a600
yogiyoazossi <chldnqja9688@naver.com>
2022-03-03 23:20:05 +0900

    Update backend.tf

--- terrform_study/4_deploy_terraform_s3/backend.tf
+++ terrform_study/4_deploy_terraform_s3/backend.tf
@@ -4,9 +4,11 @@ terraform {
     bucket = "terraofmr-up-and-running-state"
     key    = "global/s3/terraform.tfstate"
     region = "ap-northeast-2"
+
+    # 이전에 생성한 DynamoDB 테이블 이름으로 변경
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
   }
 
-  # 이전에 생성한 DynamoDB 테이블 이름으로 변경
-  dynamodb_table = "terraform-up-and-running-locks"
-  encrypt        = true
+
 }
