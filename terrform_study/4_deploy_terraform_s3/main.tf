526a5043f8ed7421accd5950f12fec6d084a5323
yogiyoazossi <chldnqja9688@naver.com>
2022-03-03 23:14:48 +0900

    2022.03.03

--- terrform_study/4_deploy_terraform_s3/main.tf
+++ terrform_study/4_deploy_terraform_s3/main.tf
@@ -7,18 +7,20 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 
   # 코드 이력을 관리하기 위해 상태 파일의 버전 관리를 활성화
-  versioning {
-    enabled = true
-  }
+// aws 모듈 버전을 업해야함. 기본 Enable로 바뀜
+//   versioning {
+//     enabled = true
+//   }
 
   # 서버 측 암호화를 활성화
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+// aws 모듈 버전을 업해야함. 기본 Enable로 바뀜  
+//   server_side_encryption_configuration {
+//     rule {
+//       apply_server_side_encryption_by_default {
+//         sse_algorithm = "AES256"
+//       }
+//     }
+//   }
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
--- terrform_study/4_deploy_terraform_s3/provider.tf
+++ terrform_study/4_deploy_terraform_s3/provider.tf
@@ -1,7 +1,3 @@
 provider "aws" {
   region = "ap-northeast-2"
 }
-
-required_providers {
-  aws = "~> 3.74"
-}
