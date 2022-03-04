667acbe6b4a8ba46772fcbce712d1d33f50feb4d
yogiyoazossi <chldnqja9688@naver.com>
2022-03-04 21:12:42 +0900

    2022.03.04

--- terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/user-data.sh
+++ terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/user-data.sh
@@ -2,8 +2,12 @@
 
 cat > index.html <<EOF
 <h1>Hello, World</h1>
-<p>DB address: ${db_address}</p>
-<p>DB port: ${db_port}</p>
+<p>DB address: ${data.terraform_remote_state.db.outputs.address}</p>
+<p>DB port: ${data.terraform_remote_state.db.outputs.port}</p>
 EOF
+
+
+# <p>DB address: ${db_address}</p>
+# <p>DB port: ${db_port}</p>
 
 nohup busybox httpd -f -p ${server_port} &
--- terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/var.tf
+++ terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/var.tf
@@ -3,15 +3,15 @@
 # You must provide a value for each of these parameters.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "db_remote_state_bucket" {
-  description = "The name of the S3 bucket used for the database's remote state storage"
-  type        = string
-}
+// variable "db_remote_state_bucket" {
+//   description = "The name of the S3 bucket used for the database's remote state storage"
+//   type        = string
+// }
 
-variable "db_remote_state_key" {
-  description = "The name of the key in the S3 bucket used for the database's remote state storage"
-  type        = string
-}
+// variable "db_remote_state_key" {
+//   description = "The name of the key in the S3 bucket used for the database's remote state storage"
+//   type        = string
+// }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
