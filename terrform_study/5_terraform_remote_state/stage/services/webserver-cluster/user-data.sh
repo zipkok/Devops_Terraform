e88dd96d400656a08f1058f997a7489dd11bb175
yogiyoazossi <chldnqja9688@naver.com>
2022-03-04 21:16:47 +0900

    2022.03.04

--- terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/user-data.sh
+++ terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/user-data.sh
@@ -2,12 +2,8 @@
 
 cat > index.html <<EOF
 <h1>Hello, World</h1>
-<p>DB address: ${data.terraform_remote_state.db.outputs.address}</p>
-<p>DB port: ${data.terraform_remote_state.db.outputs.port}</p>
+<p>DB address: ${db_address}</p>
+<p>DB port: ${db_port}</p>
 EOF
-
-
-# <p>DB address: ${db_address}</p>
-# <p>DB port: ${db_port}</p>
 
 nohup busybox httpd -f -p ${server_port} &
