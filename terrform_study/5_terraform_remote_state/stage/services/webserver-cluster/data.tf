e2197c16179d176cd1c208f4fbf9b624e3457e6d
yogiyoazossi <chldnqja9688@naver.com>
2022-03-04 21:02:57 +0900

    2022.03.04

--- terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/data.tf
+++ terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/data.tf
@@ -8,3 +8,13 @@ data "aws_vpc" "default" {
 data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
+
+data "template_file" "user_data" {
+  template = file("user-data.sh")
+
+  vars = {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  }
+}
--- terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/main.tf
+++ terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/main.tf
@@ -1,5 +1,5 @@
 # =========================================================
-# Security Group 생성
+# Security Group 생성 (ASG)
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -8,6 +8,95 @@ resource "aws_security_group" "instance" {
     to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# =========================================================
+# Security Group 생성 (ELB)
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+# =========================================================
+# Step 1/4 of AWS ELB (ALB)
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+
+  security_groups = [aws_security_group.alb.id]
+}
+
+# =========================================================
+# Step 2/4 of AWS ELB (Listener)
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # 기본 값으로 단순한 404 페이지 오류를 반환합니다.
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+# =========================================================
+# Step 3/4 of AWS ELB (Listener_rule)
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+
+# =========================================================
+# Step 4/4 of AWS ELB (Target_Group)
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 }
 
@@ -22,11 +111,7 @@ resource "aws_launch_configuration" "example" {
   # vpc_security_group_ids -> security_groups
   security_groups = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
+  user_data = data.template_file.user_data.rendered
 
   # Terraform은 기본적으로 리소스 변경 시 삭제 후 생성을 함.
   # Launch_Template 변경 시 교체 리소스를 먼저 생성하고 기존 리소스를 삭제함.
@@ -41,8 +126,12 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
 
-  min_size = 2
-  max_size = 10
+  # ELB 연결
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+  
+  min_size          = 2
+  max_size          = 10
 
   tag {
     key                 = "Name"
