f0794f5c75410eccf995eb05b3010f14b474045e
yogiyoazossi <chldnqja9688@naver.com>
2022-03-04 21:57:15 +0900

    2022.03.04

--- terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/backend.tf
+++ terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/backend.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "s3" {
-    # 이전에 생성한 Bucket 이름으로 변경
-    bucket         = "woobeom-up-and-running-state"
-    key            = "stage/services/webserver-cluster/terraform.tfstate"
-    region         = "ap-northeast-2"
+// terraform {
+//   backend "s3" {
+//     # 이전에 생성한 Bucket 이름으로 변경
+//     bucket         = "woobeom-up-and-running-state"
+//     key            = "stage/services/webserver-cluster/terraform.tfstate"
+//     region         = "ap-northeast-2"
 
-    # 이전에 생성한 DynamoDB 테이블 이름으로 변경
-    dynamodb_table = "woobeom-up-and-running-locks"
-    encrypt        = true
-  }
-}
+//     # 이전에 생성한 DynamoDB 테이블 이름으로 변경
+//     dynamodb_table = "woobeom-up-and-running-locks"
+//     encrypt        = true
+//   }
+// }
--- terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/provider.tf
+++ terrform_study/5_terraform_remote_state/stage/services/webserver-cluster/provider.tf
@@ -1,3 +1,4 @@
 provider "aws" {
     region = "ap-northeast-2"
+    version = "~> 2.0"
 }
