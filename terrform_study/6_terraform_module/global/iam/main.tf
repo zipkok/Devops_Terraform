a15061e4837e3cefa1247be12d507a5db3a4e8e4
yogiyoazossi <chldnqja9688@naver.com>
2022-03-16 11:24:57 +0900

    Update main.tf

--- terrform_study/6_terraform_module/global/iam/main.tf
+++ terrform_study/6_terraform_module/global/iam/main.tf
@@ -2,8 +2,25 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+
+# ===============================
+# count + index
+// resource "aws_iam_user" "example" {
+//   count = 3
+//   name  = "neo.${count.index}"
+// }
+
+
+
+# ===============================
+# count + index + variable
 resource "aws_iam_user" "example" {
-  count = 3
-  name  = "neo.${count.index}"
+  count = length(var.user_name)
+  name  = var.user_names[count.index]
 }
 
+variable "user_names" {
+  description = "Create IAM users with these names"
+  type        = list(string)
+  default     = ["neo", "trinity", "morpheus"]
+}
