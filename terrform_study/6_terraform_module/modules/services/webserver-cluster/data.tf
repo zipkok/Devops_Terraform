ca91022c3ff774d19503764d06b58d7c573f0c6a
yogiyoazossi <chldnqja9688@naver.com>
2022-03-16 14:01:34 +0900

    Update data.tf

--- terrform_study/6_terraform_module/modules/services/webserver-cluster/data.tf
+++ terrform_study/6_terraform_module/modules/services/webserver-cluster/data.tf
@@ -27,7 +27,17 @@ data "template_file" "user_data" {
 
   vars = {
     server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
+    db_address  = "3.3.3.3"
+    db_port     = "3306"
   }
 }
+
+// data "template_file" "user_data" {
+//   template = file("${path.module}/user-data.sh")
+
+//   vars = {
+//     server_port = var.server_port
+//     db_address  = data.terraform_remote_state.db.outputs.address
+//     db_port     = data.terraform_remote_state.db.outputs.port
+//   }
+// }
