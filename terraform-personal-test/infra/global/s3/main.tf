a504333cec4aa4b2ed8084950e98f7907e78458c
yogiyoazossi <chldnqja9688@naver.com>
2022-03-20 00:59:53 +0900

    Update main.tf

--- terraform-personal-test/infra/global/s3/main.tf
+++ terraform-personal-test/infra/global/s3/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "ap-northeast-2"
+  region = "ap-northeast-2"
 }
 
 resource "aws_s3_bucket" "bucket" {
@@ -11,6 +11,36 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+// Permissions - Block Public Access
 resource "aws_s3_bucket_public_access_block" "bucket_access" {
   bucket = aws_s3_bucket.bucket.id
+
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.example.arn}/*",
+    ]
+  }
 }
