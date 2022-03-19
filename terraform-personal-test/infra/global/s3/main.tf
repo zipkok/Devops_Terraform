bc6edb272bf5ae2932705c49f34051cc3c654aba
yogiyoazossi <chldnqja9688@naver.com>
2022-03-20 01:38:49 +0900

    Update main.tf

--- terraform-personal-test/infra/global/s3/main.tf
+++ terraform-personal-test/infra/global/s3/main.tf
@@ -29,11 +29,12 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
 data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
     principals {
-      type        = "AWS"
-      identifiers = ["123456789012"]
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
     }
+    sid       = "UniqueSidOne"
     actions   = ["s3:ListAllMyBuckets"]
-    resources = ["arn:aws:s3:::*"]
+    resources = ["arn:aws:s3:::woobeom-test-bucket/*"]
     effect    = "Allow"
   }
 }
