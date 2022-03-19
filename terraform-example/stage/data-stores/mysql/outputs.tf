637f98ce21ff44cbd4f6f96352dee9f4472741ce
yogiyoazossi <chldnqja9688@naver.com>
2022-03-19 22:52:15 +0900

    2022.03.19

--- terraform-example/stage/data-stores/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value       = aws_db_instance.example.address
-  description = "Connect to he database at this endpoint"
-}
-
-output "port" {
-  value       = aws_db_instance.example.port
-  description = "The port the database is listening on"
-}
--- terraform-example/stage/services/webserver-cluster/outputs.tf
+++
@@ -1,9 +0,0 @@
-/* data "aws_security_groups" "get_security_group" {
-  tags = {
-    Name = "woobeom"
-  }
-} */
-
-/* output "stdout_get_security_group" {
-  value = data.aws_security_groups.get_security_group.ids
-} */
