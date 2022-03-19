3a921485676b3af429450683aef9ffda65ea77b9
yogiyoazossi <chldnqja9688@naver.com>
2022-03-20 01:06:42 +0900

    Update main.tf

--- terraform-personal-test/infra/global/s3/main.tf
+++ terraform-personal-test/infra/global/s3/main.tf
@@ -28,19 +28,8 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
 
 data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["123456789012"]
-    }
-
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      aws_s3_bucket.bucket.arn,
-      "${aws_s3_bucket.bucket.arn}/*",
-    ]
+    actions   = ["s3:ListAllMyBuckets"]
+    resources = ["arn:aws:s3:::*"]
+    effect    = "Allow"
   }
 }
