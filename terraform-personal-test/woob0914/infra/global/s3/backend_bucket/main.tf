f7eeb420047578d6af02e99fddecdeec5b07690b
yogiyoazossi <chldnqja9688@naver.com>
2022-03-23 22:36:55 +0900

    Update main.tf

--- terraform-personal-test/woob0914/infra/global/s3/backend_bucket/main.tf
+++ terraform-personal-test/woob0914/infra/global/s3/backend_bucket/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "ap-northeast-2"
+  region = "ap-northeast-2"
 }
 
 resource "aws_s3_bucket" "backend_bucket" {
@@ -19,7 +19,7 @@ resource "aws_dynamodb_table" "backend_locks" {
   name         = "woobeom-terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-
+  
   attribute {
     name = "LockID"
     type = "S"
