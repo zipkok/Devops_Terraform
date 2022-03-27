b1a34646f13f3a631fb593a46dfe830f13b6a27c
yogiyoazossi <chldnqja9688@naver.com>
2022-03-28 02:20:04 +0900

    Update blog-db.tf

--- terraform-personal-test/woob0914/infra/service/blog/sg/blog-db.tf
+++ terraform-personal-test/woob0914/infra/service/blog/sg/blog-db.tf
@@ -50,13 +50,3 @@ module "blog_db" {
       cidr_blocks = "0.0.0.0/0"
   }]
 }
-
-// resource "aws_security_group_rule" "security_group_ingress_rules_test" {
-//   security_group_id        = 
-//   type                     = "egress"
-//   source_security_group_id = ["sg-0330bb85335d5115a"]
-//   description              = "egress"
-//   from_port                = 0
-//   to_port                  = 0
-//   protocol                 = "tcp"
-// }
