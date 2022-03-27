f4ba6cdad0d21964a5d50d5f7585cfc1a193eabb
yogiyoazossi <chldnqja9688@naver.com>
2022-03-27 22:51:55 +0900

    Update frontend.tf

--- terraform-personal-test/woob0914/infra/service/blog/sg/frontend.tf
+++ terraform-personal-test/woob0914/infra/service/blog/sg/frontend.tf
@@ -13,15 +13,16 @@ module "blog-frontend" {
 
   ingress_rules = [
     {
-      description = "ingress TEST"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
+      description = "HTTP open"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
       cidr_blocks = "0.0.0.0/0"
-      }, {
-      description = "ingress TEST"
-      from_port   = 0
-      to_port     = 0
+    },
+    {
+      description = "HTTPS open"
+      from_port   = 443
+      to_port     = 443
       protocol    = "tcp"
       cidr_blocks = "0.0.0.0/0"
   }]
