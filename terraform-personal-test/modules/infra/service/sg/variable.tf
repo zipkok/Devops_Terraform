f5dad7dc99308dae09a55a47d1a2bcefa10b5514
yogiyoazossi <chldnqja9688@naver.com>
2022-03-27 22:38:16 +0900

    Update variable.tf

--- terraform-personal-test/modules/infra/service/sg/variable.tf
+++ terraform-personal-test/modules/infra/service/sg/variable.tf
@@ -19,15 +19,15 @@ variable "ingress_rules" {
     to_port     = number
     protocol    = string
     cidr_blocks = string
-    })
-  }
+  }))
+}
 
-  variable "egress_rules" {
-    type = list(object({
-      description = string
-      from_port   = number
-      to_port     = number
-      protocol    = string
-      cidr_blocks = string
-      })
-    }
+variable "egress_rules" {
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = string
+  }))
+}
