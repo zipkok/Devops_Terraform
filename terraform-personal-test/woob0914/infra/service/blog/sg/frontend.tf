6951a3bb768f06b7daa9c35fc2271d2b9d598838
yogiyoazossi <chldnqja9688@naver.com>
2022-03-27 22:28:39 +0900

    Update frontend.tf

--- terraform-personal-test/woob0914/infra/service/blog/sg/frontend.tf
+++ terraform-personal-test/woob0914/infra/service/blog/sg/frontend.tf
@@ -12,33 +12,35 @@ module "frontend" {
   }
 }
 
-resource "aws_security_group_rule" "sg" {
+resource "aws_security_group_rule" "sg_ingress" {
   security_group_id = aws_security_group.frontend.id
   type              = "ingress"
-  ingress_rules = [{
-    description = "ingress TEST"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    }, {
-    description = "ingress TEST"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules = [
+    {
+      description = "ingress TEST"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      }, {
+      description = "ingress TEST"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
   }]
 }
 
-resource "aws_security_group_rule" "sg" {
+resource "aws_security_group_rule" "sg_egress" {
   security_group_id = aws_security_group.frontend.id
   type              = "egress"
-  egress_rules = [{
-    description = "egress TEST"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = [
+    {
+      description = "egress TEST"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
     },
     {
       description = "egress TEST"
