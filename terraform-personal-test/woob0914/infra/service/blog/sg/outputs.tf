88a0318db2b4b1064dd220e761fe052570239a5e
yogiyoazossi <chldnqja9688@naver.com>
2022-03-28 01:12:04 +0900

    2022.03.28

--- terraform-personal-test/woob0914/infra/service/blog/sg/outputs.tf
+++ terraform-personal-test/woob0914/infra/service/blog/sg/outputs.tf
@@ -1,4 +1,4 @@
 output "address" {
-  value       = module.blog-frontend
+  value       = module.blog-db.aws_security_group.security_group
   description = "Connect to the database at this endPoint"
 }
