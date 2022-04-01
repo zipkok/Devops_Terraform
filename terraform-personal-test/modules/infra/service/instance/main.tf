f0e2921b1ce9fd5459bea2b7f3abc8a4c0c59068
yogiyoazossi <chldnqja9688@naver.com>
2022-04-01 21:41:48 +0900

    Update main.tf

--- terraform-personal-test/modules/infra/service/instance/main.tf
+++ terraform-personal-test/modules/infra/service/instance/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = var.ec2_instance_config.ami
   instance_type = var.ec2_instance_config.instance_type
 
-  vpc_security_group_ids = vpc_security_group_ids
+  vpc_security_group_ids = var.vpc_security_group_ids
   tags                   = var.ec2_instance_tags
   monitoring             = true
 
