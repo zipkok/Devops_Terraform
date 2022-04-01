23967f596230f64d66e188241b8d2b1f1ba313cc
yogiyoazossi <chldnqja9688@naver.com>
2022-04-01 21:39:05 +0900

    Update main.tf

--- terraform-personal-test/modules/infra/service/instance/main.tf
+++ terraform-personal-test/modules/infra/service/instance/main.tf
@@ -1,8 +1,12 @@
 resource "aws_instance" "ec2" {
-  ami                    = var.ec2_instance_config.ami
-  instance_type          = var.ec2_instance_config.instance_type
-  volume_size            = var.ec2_instance_config.volume_size
-  vpc_security_group_ids = ["sg-12345678"]
+  ami           = var.ec2_instance_config.ami
+  instance_type = var.ec2_instance_config.instance_type
+
+  vpc_security_group_ids = toset(vpc_security_group_ids)
   tags                   = var.ec2_instance_tags
   monitoring             = true
+
+  root_block_device = {
+    volume_size = var.ec2_instance_config.volume_size
+  }
 }
