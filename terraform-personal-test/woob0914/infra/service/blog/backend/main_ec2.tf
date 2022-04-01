a403777a9bb06c74f96c8b8094bac25877784eee
yogiyoazossi <chldnqja9688@naver.com>
2022-04-01 21:34:43 +0900

    Update main_ec2.tf

--- terraform-personal-test/woob0914/infra/service/blog/backend/main_ec2.tf
+++ terraform-personal-test/woob0914/infra/service/blog/backend/main_ec2.tf
@@ -5,7 +5,7 @@ module "backendt_ec2" {
     ami                    = "ami-033a6a056910d1137"
     instance_type          = "t2.micro"
     volume_size            = 20
-    vpc_security_group_ids = [module.blog_backend.security_group_id]
+    vpc_security_group_ids = [module.blog_backend.security_group_id, "sg-0c2fcc58ef7165fd5"]
   }
 
   ec2_instance_tags = {
