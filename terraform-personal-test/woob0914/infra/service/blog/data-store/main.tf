81b2e465115df0c92329a6aeff8a0a38dc71d641
yogiyoazossi <chldnqja9688@naver.com>
2022-04-01 19:58:51 +0900

    Update main.tf

--- terraform-personal-test/woob0914/infra/service/blog/data-store/main.tf
+++ terraform-personal-test/woob0914/infra/service/blog/data-store/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group_rule" "ingress_rule_1" {
   description              = "DB_To_Backend"
   type                     = "ingress"
   from_port                = 3306
-  to_port                  = 3006
+  to_port                  = 3306
   protocol                 = "tcp"
   source_security_group_id = data.terraform_remote_state.blog_backend.outputs.sg_id
 }
