230d555054e7d2284249d5d3945af9a6ef03e185
yogiyoazossi <chldnqja9688@naver.com>
2022-04-01 18:28:45 +0900

    Update main.tf

--- terraform-personal-test/woob0914/infra/service/blog/frontend/main.tf
+++ terraform-personal-test/woob0914/infra/service/blog/frontend/main.tf
@@ -4,12 +4,12 @@ output "blog_frontend" {
 }
 
 resource "aws_security_group_rule" "backend_ingress" {
+  security_group_id = module.blog_frontend.security_group_id
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  security_group_id = module.blog_frontend.security_group_id
-  cidr_blocks       = "0.0.0.0/0"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 module "blog_frontend" {
