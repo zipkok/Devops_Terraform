73d1c17836db046edab2995058b3e0d248a3a54b
yogiyoazossi <chldnqja9688@naver.com>
2022-04-01 19:46:25 +0900

    2022.04.01

--- terraform-personal-test/woob0914/infra/service/blog/backend/main.tf
+++ terraform-personal-test/woob0914/infra/service/blog/backend/main.tf
@@ -3,45 +3,45 @@ output "sg_id" {
   description = "Security Group ID"
 }
 
-resource "aws_security_group_rule" "ingress_rule_1" {
-  security_group_id        = module.blog_backend.security_group_id
-  description              = "Frontend -> Backend"
-  type                     = "ingress"
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  source_security_group_id = data.terraform_remote_state.blog_frontend.outputs.sg_id
-}
+// resource "aws_security_group_rule" "ingress_rule_1" {
+//   security_group_id        = module.blog_backend.security_group_id
+//   description              = "Frontend -> Backend"
+//   type                     = "ingress"
+//   from_port                = 8080
+//   to_port                  = 8080
+//   protocol                 = "tcp"
+//   source_security_group_id = data.terraform_remote_state.blog_frontend.outputs.sg_id
+// }
 
-resource "aws_security_group_rule" "egress_rule_1" {
-  security_group_id        = module.blog_frontend.security_group_id
-  description              = "Backend -> Frontend"
-  type                     = "egress"
-  from_port                = 1025
-  to_port                  = 65535
-  protocol                 = "tcp"
-  source_security_group_id = data.terraform_remote_state.blog_frontend.outputs.sg_id
-}
+// resource "aws_security_group_rule" "egress_rule_1" {
+//   security_group_id        = module.blog_frontend.security_group_id
+//   description              = "Backend -> Frontend"
+//   type                     = "egress"
+//   from_port                = 1025
+//   to_port                  = 65535
+//   protocol                 = "tcp"
+//   source_security_group_id = data.terraform_remote_state.blog_frontend.outputs.sg_id
+// }
 
-resource "aws_security_group_rule" "ingress_rule_2" {
-  security_group_id        = module.blog_backend.security_group_id
-  description              = "DB -> Backend"
-  type                     = "ingress"
-  from_port                = 1025
-  to_port                  = 65535
-  protocol                 = "tcp"
-  source_security_group_id = data.terraform_remote_state.blog_db.outputs.sg_id
-}
+// resource "aws_security_group_rule" "ingress_rule_2" {
+//   security_group_id        = module.blog_backend.security_group_id
+//   description              = "DB -> Backend"
+//   type                     = "ingress"
+//   from_port                = 1025
+//   to_port                  = 65535
+//   protocol                 = "tcp"
+//   source_security_group_id = data.terraform_remote_state.blog_db.outputs.sg_id
+// }
 
-resource "aws_security_group_rule" "egress_rule_2" {
-  security_group_id        = module.blog_frontend.security_group_id
-  description              = "Backend -> DB"
-  type                     = "egress"
-  from_port                = 3306
-  to_port                  = 3306
-  protocol                 = "tcp"
-  source_security_group_id = data.terraform_remote_state.blog_db.outputs.sg_id
-}
+// resource "aws_security_group_rule" "egress_rule_2" {
+//   security_group_id        = module.blog_frontend.security_group_id
+//   description              = "Backend -> DB"
+//   type                     = "egress"
+//   from_port                = 3306
+//   to_port                  = 3306
+//   protocol                 = "tcp"
+//   source_security_group_id = data.terraform_remote_state.blog_db.outputs.sg_id
+// }
 
 
 
--- terraform-personal-test/woob0914/infra/service/blog/frontend/main.tf
+++ terraform-personal-test/woob0914/infra/service/blog/frontend/main.tf
@@ -3,23 +3,23 @@ output "sg_id" {
   description = "Security Group ID"
 }
 
-resource "aws_security_group_rule" "ingress_rule_1" {
-  security_group_id        = module.blog_frontend.security_group_id
-  type                     = "ingress"
-  from_port                = 1025
-  to_port                  = 65535
-  protocol                 = "tcp"
-  source_security_group_id = data.terraform_remote_state.blog_backend.outputs.sg_id
-}
+// resource "aws_security_group_rule" "ingress_rule_1" {
+//   security_group_id        = module.blog_frontend.security_group_id
+//   type                     = "ingress"
+//   from_port                = 1025
+//   to_port                  = 65535
+//   protocol                 = "tcp"
+//   source_security_group_id = data.terraform_remote_state.blog_backend.outputs.sg_id
+// }
 
-resource "aws_security_group_rule" "egress_rule_1" {
-  security_group_id        = module.blog_frontend.security_group_id
-  type                     = "egress"
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  source_security_group_id = data.terraform_remote_state.blog_backend.outputs.sg_id
-}
+// resource "aws_security_group_rule" "egress_rule_1" {
+//   security_group_id        = module.blog_frontend.security_group_id
+//   type                     = "egress"
+//   from_port                = 8080
+//   to_port                  = 8080
+//   protocol                 = "tcp"
+//   source_security_group_id = data.terraform_remote_state.blog_backend.outputs.sg_id
+// }
 
 module "blog_frontend" {
   source = "../../../../../modules/infra/service/sg"
