2e2d842769a748aa2a6d9a8ab45a2253e4a3b6a2
yogiyoazossi <chldnqja9688@naver.com>
2022-05-09 02:28:23 +0900

    Update outputs.tf

--- terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/outputs.tf
+++ terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/outputs.tf
@@ -1,6 +1,6 @@
 output "aws_instance_id" {
     description = "Instance ID"
-    value = module.blog-webapp.oup_aws_instance_id["woobt-tfinf006"]
+    value = module.blog-webapp.oup_aws_instance_id[0]
 }
 
 // output "aws_instance_id" {
