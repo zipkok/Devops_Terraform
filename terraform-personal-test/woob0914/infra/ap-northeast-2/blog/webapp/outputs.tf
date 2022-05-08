3085ac179f6c663e3144907f239424afadaaba68
yogiyoazossi <chldnqja9688@naver.com>
2022-05-09 03:52:02 +0900

    Update outputs.tf

--- terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/outputs.tf
+++ terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/outputs.tf
@@ -4,5 +4,15 @@
 // }
 
 output "aws_instance_id" {
-  value = [ for hostname in module.blog-webapp.oup_aws_instance[0] : hostname.id ]
+  value = [for hostname in module.blog-webapp.oup_aws_instance[0] : hostname.id]
+}
+
+data "aws_instance" "aws_instance_id" {
+  filter {
+    name   = "service"
+    values = ["media"]
+} 
+
+output "test" {
+  value = data.aws_instance.aws_instance_id
 }
