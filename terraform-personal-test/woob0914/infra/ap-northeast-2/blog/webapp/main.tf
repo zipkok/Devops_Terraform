0498ad5b4cfbade69bad7106e9b5dc31824c93af
yogiyoazossi <chldnqja9688@naver.com>
2022-05-09 01:43:09 +0900

    2022.05.09

--- terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/main.tf
+++ terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/main.tf
@@ -1,7 +1,7 @@
 // subnet을 loop돌리니까, ec2_instance_name이 영향 받음. 서로 각자 loop가 실행됨..
 module "blog-webapp" {
   source                      = "../../../../../modules/infra/service/aws_instance"
-  ec2_instance_name           = ["wakrl-rxmda001", "wakrl-rxmda002", "wakrl-rxmda004", "wakrl-rxmda005"]
+  ec2_instance_name           = ["woobt-tfinf001", "woobt-tfinf002", "woobt-tfinf004", "woobt-tfinf005"]
   ec2_instance_security_group = ["sg-0fa0d3359fec65113", "sg-084a08cc5928568e8"]
   vpc_ec2_subnet_id           = ["subnet-095041c85bc79d384", "subnet-05a0afbf6beb61357"]
 
@@ -31,53 +31,6 @@ module "blog-webapp" {
     terraform = "true"
   }
 }
-
-
-
-// resource " aws_instance " " my-app1 " {
-//   ami                         = " ami-0cbec04a61be382d9 "
-//   availability_zone           = " ap-northeast-2a "
-//   ebs_optimized               = false
-//   instance_type               = " t2.micro "
-//   monitoring                  = false
-//   key_name                    = " woob0914 "
-//   subnet_id                   = " subnet-095041c85bc79d384 "
-//   vpc_security_group_ids      = [" sg-0979156848f949963 "]
-//   associate_public_ip_address = true
-//   source_dest_check           = true
-
-//   root_block_device {
-//     volume_type           = " gp2 "
-//     volume_size           = 8
-//     delete_on_termination = true
-//   }
-
-//   tags = {
-//     Name = " my-app1 "
-//   }
-// }
-
-// resource " aws_instance " " my-app2 " {
-//   ami                         = " ami-0cbec04a61be382d9 "
-//   availability_zone           = " ap-northeast-2a "
-//   ebs_optimized               = false
-//   instance_type               = " t2.micro "
-//   monitoring                  = false
-//   key_name                    = " woob0914 "
-//   subnet_id                   = " subnet-095041c85bc79d384 "
-//   vpc_security_group_ids      = [" sg-0979156848f949963 "]
-//   associate_public_ip_address = true
-
-//   root_block_device {
-//     volume_type           = " gp2 "
-//     volume_size           = 8
-//     delete_on_termination = true
-//   }
-
-//   tags = {
-//     Name = " my-app2 "
-//   }
-// }
 
 
 // resource " aws_alb " " external-web-lb " {
