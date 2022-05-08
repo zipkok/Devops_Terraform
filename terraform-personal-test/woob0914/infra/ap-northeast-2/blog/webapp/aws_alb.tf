c5764ab648df720581895b7626eb42ccff1b29db
yogiyoazossi <chldnqja9688@naver.com>
2022-05-09 03:42:49 +0900

    Update aws_alb.tf

--- terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/aws_alb.tf
+++ terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/aws_alb.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "rsc_elb_any_80" {
   }
 
   egress {
-    from_port   = 1025
+    from_port   = 80
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
