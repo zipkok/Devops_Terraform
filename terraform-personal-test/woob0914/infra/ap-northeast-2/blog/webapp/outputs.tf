32b8b9eace154f4047b17416fe35e34e328bbc4f
yogiyoazossi <chldnqja9688@naver.com>
2022-05-09 02:27:33 +0900

    Update outputs.tf

--- terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/outputs.tf
+++ terraform-personal-test/woob0914/infra/ap-northeast-2/blog/webapp/outputs.tf
@@ -1,8 +1,8 @@
-// output "aws_instance_id" {
-//     description = "Instance ID"
-//     value = module.blog-webapp.oup_aws_instance_id[*].*.arn
-// }
-
 output "aws_instance_id" {
-  value = [ for vpc in module.blog-webapp.oup_aws_instance_id.* : vpc ]
+    description = "Instance ID"
+    value = module.blog-webapp.oup_aws_instance_id["woobt-tfinf006"]
 }
+
+// output "aws_instance_id" {
+//   value = [ for vpc in module.blog-webapp.oup_aws_instance_id[*] : vpc ]
+// }
