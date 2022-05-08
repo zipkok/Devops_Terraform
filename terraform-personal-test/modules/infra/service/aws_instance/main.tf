dc9171870eb3edb2696592f7a3551831a90a8c01
yogiyoazossi <chldnqja9688@naver.com>
2022-05-09 00:53:24 +0900

    Update main.tf

--- terraform-personal-test/modules/infra/service/aws_instance/main.tf
+++ terraform-personal-test/modules/infra/service/aws_instance/main.tf
@@ -9,32 +9,32 @@ resource "aws_instance" "mdu_instanceTemplate" {
 
   // subnet_id = var.vpc_ec2_subnet_id
   // subnet_id = element(var.vpc_ec2_subnet_id, length(var.ec2_instance_name) % 2) 
-subnet_id = element(split(",", var.subnet_id), length(var.ec2_instance_name)) }
+  subnet_id = element(split(",", var.subnet_id), length(var.ec2_instance_name))
 
-user_data = <<-EOF
+  user_data = <<-EOF
             #!/bin/bash
             hostnamectl set-hostname ${each.value}
             EOF
 
-tags = {
-  Name         = each.value
-  Creator      = var.ec2_instance_tags.creator
-  Team         = var.ec2_instance_tags.team
-  Project      = var.ec2_instance_tags.project
-  Service      = var.ec2_instance_tags.service
-  UseTerraform = "true"
-}
+  tags = {
+    Name         = each.value
+    Creator      = var.ec2_instance_tags.creator
+    Team         = var.ec2_instance_tags.team
+    Project      = var.ec2_instance_tags.project
+    Service      = var.ec2_instance_tags.service
+    UseTerraform = "true"
+  }
 
-root_block_device {
-  volume_size           = var.ec2_root_volume.volume_size
-  delete_on_termination = var.ec2_root_volume.delete_on_termination
-  volume_type           = var.ec2_root_volume.volume_type
-}
+  root_block_device {
+    volume_size           = var.ec2_root_volume.volume_size
+    delete_on_termination = var.ec2_root_volume.delete_on_termination
+    volume_type           = var.ec2_root_volume.volume_type
+  }
 
-ebs_block_device {
-  device_name           = "/dev/sdb"
-  volume_size           = var.ec2_data_volume.volume_size
-  delete_on_termination = var.ec2_data_volume.delete_on_termination
-  volume_type           = var.ec2_data_volume.volume_type
-}
+  ebs_block_device {
+    device_name           = "/dev/sdb"
+    volume_size           = var.ec2_data_volume.volume_size
+    delete_on_termination = var.ec2_data_volume.delete_on_termination
+    volume_type           = var.ec2_data_volume.volume_type
+  }
 }
